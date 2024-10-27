a5d409634455a2c7d0d66f37eebd00e0c521bda4
voidAasif <aasifsaifi9280@gmail.com>
2024-10-27 12:16:26 +0530

    basic data flow bw html and java

--- j001_first/src/main/java/com/example/j001_first/HomeController.java
+++ j001_first/src/main/java/com/example/j001_first/HomeController.java
@@ -1,22 +1,27 @@
 package com.example.j001_first;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
     
-    @GetMapping("/")
+    @GetMapping("/") //home mapping (default url);
     public String home(){
         System.out.println("Home fun");
-        return "index";
+        return "index"; // Return index.html;
     }
 
-    @ResponseBody
-    @GetMapping("/test")
-    public String test(){
-        System.out.println("Test fun");
-        return "Working";
+    @PostMapping("/post") //executed when form submitted;
+    public String getDataFromIndexInputs(@RequestParam("input1") String input1, //RequestParam annotation get data from Form and set into variable;
+                                         @RequestParam("input2") String input2,
+                                         Model model) { //model is used to set data on html page;
+        
+        System.out.println("Received: " + input1 + " " + input2);
+        model.addAttribute("message", "Data Submitted Successfully"); //set data on 'message' attribute in result.html bcz it return result.html;
+        return "result"; // A view to show the result or confirmation message
     }
 }
