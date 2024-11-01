bbcf530981ba85eedc22574ca28615cec1fbe010
voidAasif <aasifsaifi9280@gmail.com>
2024-11-01 20:12:58 +0530

    init new project

---
+++ j002_GPPD/src/main/java/com/example/j002_GPPD/J002GppdApplication.java
@@ -0,0 +1,13 @@
+package com.example.j002_GPPD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J002GppdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J002GppdApplication.class, args);
+	}
+
+}
