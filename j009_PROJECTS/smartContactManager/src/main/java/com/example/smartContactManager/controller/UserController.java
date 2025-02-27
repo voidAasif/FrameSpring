c3fdc9f594cd873bc011d7e8f2d68a977c9ebf07
voidAasif <aasifsaifi9280@gmail.com>
2025-02-27 17:11:29 +0530

    process contact without DB

--- j009_PROJECTS/smartContactManager/src/main/java/com/example/smartContactManager/controller/UserController.java
+++ j009_PROJECTS/smartContactManager/src/main/java/com/example/smartContactManager/controller/UserController.java
@@ -5,8 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.example.smartContactManager.dao.UserRepository;
+import com.example.smartContactManager.entities.Contact;
 import com.example.smartContactManager.entities.User;
 
 @Controller
@@ -16,16 +22,34 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/dashboard")
-    public String dashboard(Model model, Principal principal){
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal){
         String username = principal.getName();
 
         User user = userRepository.getUserByUserName(username);
 
         System.out.println(user);
-
+        
         model.addAttribute("user", user);
-
+    }
+    
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal){
+        model.addAttribute("title", "Dashboard");
         return "normal/userDashboard";
+    }
+
+    @GetMapping("/addContact")
+    public String addContact(Model model){
+        model.addAttribute("title", "Add Contact");
+        model.addAttribute("contact", new Contact());
+        return "normal/addContact";
+    }
+
+    @PostMapping("/process_contact")
+    public String processContact(@ModelAttribute("contact") Contact contact, @RequestParam("imagee") MultipartFile file){
+        contact.setImage(file.getOriginalFilename());
+        System.out.println(contact);
+        return "normal/addContact";
     }
 }
