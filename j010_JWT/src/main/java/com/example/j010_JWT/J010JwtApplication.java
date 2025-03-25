6be8ed06fd8c4ef74b1213afe3b288539862e427
voidAasif <aasifsaifi9280@gmail.com>
2025-03-25 12:29:57 +0530

    init JWT

---
+++ j010_JWT/src/main/java/com/example/j010_JWT/J010JwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.j010_JWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J010JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J010JwtApplication.class, args);
+	}
+
+}
---
+++ j010_JWT/src/test/java/com/example/j010_JWT/J010JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.j010_JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J010JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
